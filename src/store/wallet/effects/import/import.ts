e190fea4255a08c57a7b327a3750b65d5e329947
genghaifei <genghaifei@canaan-creative.com>
2023-04-23 20:25:44 +0800

    1.创建观察钱包完毕。测试完毕 2.如果存在相同的key，提示后不予导入

--- src/store/wallet/effects/import/import.ts
+++ src/store/wallet/effects/import/import.ts
@@ -1426,25 +1426,26 @@ export const startImportWithDerivationPath =
           const matchedKey = getMatchedKey(_key, Object.values(WALLET.keys));
           console.log("---------- 使用公钥导入: matchedKey", JSON.stringify(matchedKey));
           console.log("---------- 使用公钥导入: currencyAbbreviation currencyName tokenOpts ", currencyAbbreviation, currencyName, JSON.stringify(tokenOpts));
-          // if (matchedKey) {
-          //   // To avoid duplicate key creation when importing
-          //   wallet.credentials.keyId = wallet.keyId = matchedKey.id;
-          //   key = await findKeyByKeyId(matchedKey.id, WALLET.keys);
-          //   key.wallets.push(
-          //     merge(
-          //       wallet,
-          //       buildWalletObj(
-          //         {
-          //           ...wallet.credentials,
-          //           currencyAbbreviation,
-          //           currencyName,
-          //         },
-          //         tokenOpts,
-          //       ),
-          //     ),
-          //   );
-          //   console.log("---------- 使用公钥导入: if 最后的key ", JSON.stringify(key));
-          // } else {
+          if (matchedKey) {
+            return reject(new Error(t('Wallet already exists')));
+            // To avoid duplicate key creation when importing
+            // wallet.credentials.keyId = wallet.keyId = matchedKey.id;
+            // key = await findKeyByKeyId(matchedKey.id, WALLET.keys);
+            // key.wallets.push(
+            //   merge(
+            //     wallet,
+            //     buildWalletObj(
+            //       {
+            //         ...wallet.credentials,
+            //         currencyAbbreviation,
+            //         currencyName,
+            //       },
+            //       tokenOpts,
+            //     ),
+            //   ),
+            // );
+            // console.log("---------- 使用公钥导入: if 最后的key ", JSON.stringify(key));
+          } else {
             key = buildKeyObj({
               key: _key,
               wallets: [
@@ -1459,7 +1460,7 @@ export const startImportWithDerivationPath =
               backupComplete: true,
             });
             console.log("---------- 使用公钥导入: else 最后的key ", JSON.stringify(key));
-          // }
+          }
           dispatch(
             successImport({
               key,
