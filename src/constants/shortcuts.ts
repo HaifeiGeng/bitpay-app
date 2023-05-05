1793afca51f7b906bb03511d7d0843e4b3762b20
genghaifei <genghaifei@canaan-creative.com>
2023-05-05 15:28:42 +0800

    1.移除无关的Shortcut

--- src/constants/shortcuts.ts
+++ src/constants/shortcuts.ts
@@ -2,18 +2,18 @@ import {Platform} from 'react-native';
 import {ShortcutItem} from 'react-native-quick-actions';
 
 export const ShortcutList: Array<ShortcutItem> = [
-  {
-    type: 'buy',
-    title: 'Buy Crypto',
-    icon: Platform.OS === 'ios' ? 'BuyCrypto' : 'ic_shortcut_buy_crypto',
-    userInfo: {url: ''},
-  },
-  {
-    type: 'swap',
-    title: 'Exchange',
-    icon: Platform.OS === 'ios' ? 'SwapCrypto' : 'ic_shortcut_swap_crypto',
-    userInfo: {url: ''},
-  },
+  // {
+  //   type: 'buy',
+  //   title: 'Buy Crypto',
+  //   icon: Platform.OS === 'ios' ? 'BuyCrypto' : 'ic_shortcut_buy_crypto',
+  //   userInfo: {url: ''},
+  // },
+  // {
+  //   type: 'swap',
+  //   title: 'Exchange',
+  //   icon: Platform.OS === 'ios' ? 'SwapCrypto' : 'ic_shortcut_swap_crypto',
+  //   userInfo: {url: ''},
+  // },
   {
     type: 'receive',
     title: 'Receive',
@@ -27,10 +27,10 @@ export const ShortcutList: Array<ShortcutItem> = [
     icon: Platform.OS === 'ios' ? 'SendCrypto' : 'ic_shortcut_send_crypto',
     userInfo: {url: ''},
   },
-  {
-    type: 'share',
-    title: 'Share App',
-    icon: Platform.OS === 'ios' ? 'Share' : 'ic_shortcut_share_app',
-    userInfo: {url: ''},
-  },
+  // {
+  //   type: 'share',
+  //   title: 'Share App',
+  //   icon: Platform.OS === 'ios' ? 'Share' : 'ic_shortcut_share_app',
+  //   userInfo: {url: ''},
+  // },
 ];
